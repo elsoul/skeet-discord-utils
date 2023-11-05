b43608fe70653c82c5a06cdc8d70431c1b3d3a26
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-05 11:41:15 +0100

    add CI test

--- src/lib/getChangeLog.ts
+++ src/lib/getChangeLog.ts
@@ -1,9 +1,14 @@
 import { execSync } from 'child_process'
+import { messageChannel } from './messageChannel'
 
-export function getChangeLog(repoName = 'elsoul/skeet-discord-utils') {
+export const discordChangeLog = async (
+  repoName = 'elsoul/skeet-discord-utils',
+) => {
   try {
     const log = execSync(`gh release view --repo ${repoName}`).toString()
-
+    const token = process.env.DISCORD_TOKEN || ''
+    const channelId = process.env.DISCORD_CHANGELOG_CHANNEL_ID || ''
+    await messageChannel(token, channelId, log)
     return log
   } catch (error) {
     console.log(`Error in getChangeLog: ${error}`)
@@ -11,5 +16,5 @@ export function getChangeLog(repoName = 'elsoul/skeet-discord-utils') {
   }
 }
 
-const res = getChangeLog()
+const res = discordChangeLog()
 console.log(res)
