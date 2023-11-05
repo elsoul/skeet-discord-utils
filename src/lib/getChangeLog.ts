d17e32dea2a5dbe1c232b1116d0bf99de84df7bf
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-05 11:30:50 +0100

    add CI

--- src/lib/getChangeLog.ts
+++ src/lib/getChangeLog.ts
@@ -1,39 +1,10 @@
 import { execSync } from 'child_process'
 
-export function getChangeLog() {
+export function getChangeLog(repoName = 'elsoul/skeet-discord-utils') {
   try {
-    const remoteURL = execSync('git remote get-url origin').toString().trim()
+    const log = execSync(`gh release view --repo ${repoName}`).toString()
 
-    console.log(`remoteURL: ${remoteURL}`)
-    const matchResult = remoteURL.match(
-      /github\.com[/:]([^/]+)\/([^/]+?)(?:\.git)?$/,
-    )
-    console.log(`matchResult: ${matchResult}`)
-    if (!matchResult) {
-      throw new Error('Could not extract repository owner and name.')
-    }
-
-    const repositoryOwner = matchResult[1]
-    const repositoryName = matchResult[2]
-
-    const log = execSync(
-      `git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:"- %s by @%an in #%h"`,
-    ).toString()
-    const lines = log.split('\n')
-    const githubCommitURL = `https://github.com/${repositoryOwner}/${repositoryName}/commit`
-    const formattedLines = lines.map((line) => {
-      if (line.startsWith('Merge pull request')) {
-        return line
-      } else {
-        return line.replace(/#(\w+)/g, (match) => {
-          const shortHash = match.substring(1)
-          return `${githubCommitURL}/${shortHash}`
-        })
-      }
-    })
-    const formattedLog = formattedLines.join('\n')
-
-    return `## What's Changed\n\n${formattedLog}`
+    return log
   } catch (error) {
     console.log(`Error in getChangeLog: ${error}`)
     return ''
