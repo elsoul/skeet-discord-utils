f1151c125a3fd5d50a816ea4ae2ec044079ad5c0
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-06 10:42:17 +0100

    add discord notification in Github Actions

--- src/runDiscordChangeLog.ts
+++ src/runDiscordChangeLog.ts
@@ -8,8 +8,10 @@ const run = async () => {
   const token = process.env.DISCORD_TOKEN || ''
   const channelId = process.env.DISCORD_CHANNEL_ID || ''
   const channelIdJA = process.env.DISCORD_CHANNEL_ID_JA || ''
+  const laboSkeetChannelId = process.env.LABO_SKEET_CHANNEL_ID || ''
   await discordChangeLog(token, REPO_NAME, [channelId])
   await discordChangeLog(token, REPO_NAME, [channelIdJA], 'ja')
+  await discordChangeLog(token, REPO_NAME, [laboSkeetChannelId])
 }
 
 run()
