3d8a990f28513b91ad21ddf0bb04532f768e56c8
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-11-13 18:33:04 +0100

    update: release v0.2.10

--- dist/lib/deployCommands.d.ts
+++ dist/lib/deployCommands.d.ts
@@ -35,4 +35,4 @@ import { SlashCommandBuilder } from 'discord.js';
  *   .catch((error) => console.error(error));
  * ```
  */
-export declare function deployCommands(token: string, discordClientId: string, guildId: string, commands: Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>): Promise<void>;
+export declare function deployCommands(token: string, discordClientId: string, guildId: string, commands: Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>[]): Promise<void>;
